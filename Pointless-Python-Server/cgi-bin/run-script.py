0a1a0200bf8eed8b5e032948b98304012de064ab
Test <test@example.com>
2013-09-21 10:17:03 +0000

    Workable form & python script to run user team's script in a 'safe' shell. Execution stats are accumulated for processing into team positions.

--- Pointless-Python-Server/cgi-bin/run-script.py
+++ Pointless-Python-Server/cgi-bin/run-script.py
@@ -2,18 +2,45 @@
  
 import cgi
 import sys
- 
+import stat
+import subprocess
+import datetime
+import os
+import re
+
+# Get the user's request
 form = cgi.FieldStorage()
  
 teamname = form.getvalue('name')
-script = form.getvalue('script')
+script = re.sub(r"\r\n", "\n", form.getvalue('script'))
 
-# 'Normal' way to log?
-sys.stderr.write('run-script: %s\n%s\n' % (teamname, script))
+# Switch to the scratch directory
+os.chdir('/home/erac/scratch')
+
+# Clean up scratch directory
+subprocess.check_output('rm -rf *', shell=True)
+
+# Write script to a file
+f = open('script', 'w')
+f.write(script)
+f.close()
+os.chmod('script', stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+# Run the user's script
+now = datetime.datetime.now()
+
+start = os.times()
+output = subprocess.check_output('./script', shell=True)
+end = os.times()
+delta = end[4] - start[4]
 
 # 'increment' the appropriate team counter
+f = open('/home/erac/team-stats', 'a')
+f.write('%s,%s,%.2f\n' % (now, teamname, delta))
+f.close()
 
-# run the script ()
+# A bit of logging
+sys.stderr.write('Script for [%s] (elapsed %.2f):\n%s\n\n' % (teamname, delta, script))
 
 print """Content-type: text/html
 
@@ -22,6 +49,18 @@ print """Content-type: text/html
     <title>run-script</title>
   </head>
   <body>
-    Arguments are: %s %s
+    <h3>Request for team %s is:</h3>
+    <hr/>
+    <pre>
+%s
+    </pre>
+    <hr/>
+    <h3>Output is:</h3>
+    <hr/>
+    <pre>
+%s
+    </pre>
+    <hr/>
+    Elapsed time: %.2f
   </body>
-</html>""" % (teamname, script)
+</html>""" % (teamname, script, output, delta)
